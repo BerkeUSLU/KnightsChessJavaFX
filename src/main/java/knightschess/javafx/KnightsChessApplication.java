98b832020e2da117bd83968dd36c83e0821b4feb
Ibrahim Berke USLU <quitwin963@gmail.com>
2021-05-28 00:40:53 +0200

    launch.fxml is updated

--- src/main/java/knightschess/javafx/KnightsChessApplication.java
+++ src/main/java/knightschess/javafx/KnightsChessApplication.java
@@ -1,4 +1,17 @@
 package knightschess.javafx;
 
-public class KnightsChessApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import util.javafx.ControllerHelper;
+
+public class KnightsChessApplication extends Application {
+    private FXMLLoader fxmlLoader = new FXMLLoader();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Flipping Coins");
+        stage.setResizable(false);
+        ControllerHelper.loadAndShowFXML(fxmlLoader,"/fxml/launch.fxml",stage);
+    }
 }
