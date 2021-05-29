cafd94ed8b5c1adf9e4d37a755cbeb56988b8e3a
Ibrahim Berke USLU <quitwin963@gmail.com>
2021-05-29 18:10:22 +0200

    PlayerState model class is created

---
+++ src/main/java/knightschess/model/PlayerState.java
@@ -0,0 +1,19 @@
+package knightschess.model;
+
+import javafx.scene.image.ImageView;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PlayerState {
+    private List<Pair> moveList;
+    private List<ImageView> imageViewList;
+    private boolean isPlayer1Turn;
+    public PlayerState(){
+        moveList = new ArrayList<>();
+        imageViewList = new ArrayList<>();
+        isPlayer1Turn = true;
+    }
+}
