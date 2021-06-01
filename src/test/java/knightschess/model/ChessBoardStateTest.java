3700d0b6505bb32e6314ef31b2cb8187ce39a78c
Ibrahim Berke USLU <quitwin963@gmail.com>
2021-06-01 12:31:05 +0200

    ChessBoardStateTest is updated

--- src/test/java/knightschess/model/ChessBoardStateTest.java
+++ src/test/java/knightschess/model/ChessBoardStateTest.java
@@ -58,4 +58,53 @@ class ChessBoardStateTest {
 
         assertTrue(chessBoardState.possibleMoves.containsAll(expectedPairs));
     }
+    @Test
+    void changeKnightState() {
+        ChessBoardState chessBoardState = new ChessBoardState();
+        PlayerState playerState = new PlayerState();
+        chessBoardState.initializeBoard();
+
+        Pair posPair = new Pair(3,3);
+        int row = 1;
+        int col = 2;
+        chessBoardState.changeKnightState(playerState,row,col,posPair);
+
+        assertEquals(2,chessBoardState.chessBoard.get(row).get(col));
+        assertEquals(1,chessBoardState.chessBoard.get(posPair.getRow()).get(posPair.getColumn()));
+
+        playerState.setPlayer1Turn(false);
+        posPair = new Pair(4,4);
+        row = 3;
+        col = 6;
+        chessBoardState.changeKnightState(playerState,row,col,posPair);
+
+        assertEquals(3,chessBoardState.chessBoard.get(row).get(col));
+        assertEquals(1,chessBoardState.chessBoard.get(posPair.getRow()).get(posPair.getColumn()));
+
+    }
+
+    @Test
+    void isKnightMoveValid() {
+        ChessBoardState chessBoardState = new ChessBoardState();
+        PlayerState playerState = new PlayerState();
+        chessBoardState.initializeBoard();
+        chessBoardState.possibleMoves.add(new Pair(3,3));
+        playerState.getMoveList().add(new Pair(1,2));
+
+        int row = 3;
+        int col = 3;
+        assertTrue(chessBoardState.isKnightMoveValid(playerState,row,col));
+
+        assertFalse(chessBoardState.isKnightMoveValid(playerState,row,col));
+
+        chessBoardState.chessBoard.get(3).set(3,1);
+        assertFalse(chessBoardState.isKnightMoveValid(playerState,row,col));
+
+        chessBoardState.chessBoard.get(3).set(3,3);
+        assertFalse(chessBoardState.isKnightMoveValid(playerState,row,col));
+
+        row = 2;
+        col = 2;
+        assertFalse(chessBoardState.isKnightMoveValid(playerState,row,col));
+    }
 }
